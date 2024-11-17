b182c451e1ae562c5b616441bfdc10e58a844921
Botto Renato Daniel <146925688+BottoRenatoDaniel@users.noreply.github.com>
2024-11-17 18:54:25 +0100

    Add files via upload

---
+++ loginpage/login.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    const loadingScreen = document.getElementById('loading-screen');
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+    }, 5500);
+};
